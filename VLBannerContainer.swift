82fa667346189e3e700a18289b4f3e1bf9fd5982
Victor Leach <victor96@mac.com>
2015-11-06 14:14:53 +0000

    Added container view controller

---
+++ VLBannerContainer.swift
@@ -0,0 +1,23 @@
+//
+//  ContainerController.swift
+//  UkuleleTabs
+//
+//  Created by Victor on 08/09/2015.
+//  Copyright (c) 2015 Vmlweb. All rights reserved.
+//
+
+import UIKit
+
+class VLBannerContainer: UIViewController{
+	var subControllers = [UIViewController]()
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
+		for subController in subControllers{
+			if VLBanner.shared.iapPurchased{
+				subController.view.frame.size.height = self.view.frame.size.height
+			}else{
+				subController.viewWillLayoutSubviews()
+			}
+		}
+	}
+}
